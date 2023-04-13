4f5e53fcba2cce3431deb66e134a3e62556574cc
YAGO ABREU <yagog4briel@gmail.com>
2023-04-13 00:20:06 -0300

    API

---
+++ src/test/java/yago/web/api/MyFirstWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package yago.web.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
